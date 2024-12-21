adb464c88163d7298acdd1fc7f8037936c62ab76
Daniel Morshinin <geelix@mail.ru>
2024-12-21 23:41:47 +0300

    init commit

---
+++ blogicum/blog/migrations/0002_auto.py
@@ -0,0 +1,40 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='post',
+            options={'default_related_name': 'posts', 'ordering': ('-pub_date',), 'verbose_name': 'публикация', 'verbose_name_plural': 'Публикации'},
+        ),
+        migrations.AlterField(
+            model_name='category',
+            name='description',
+            field=models.TextField(default=True, verbose_name='Описание'),
+        ),
+        migrations.AlterField(
+            model_name='category',
+            name='is_published',
+            field=models.BooleanField(default=True, help_text='Снимите галочку, чтобы скрыть публикацию.', verbose_name='Опубликовано'),
+        ),
+        migrations.AlterField(
+            model_name='location',
+            name='is_published',
+            field=models.BooleanField(default=True, help_text='Снимите галочку, чтобы скрыть публикацию.', verbose_name='Опубликовано'),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='is_published',
+            field=models.BooleanField(default=True, help_text='Снимите галочку, чтобы скрыть публикацию.', verbose_name='Опубликовано'),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='title',
+            field=models.CharField(max_length=256),
+        ),
+    ]
